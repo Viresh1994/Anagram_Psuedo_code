2462abaadd8ce7b7c704efa02ead27bb8b876362
Viresh1994 <vireshanadakatni@gmail.com>
2022-10-04 09:41:43 +0530

    program update

--- Anagram.java
+++ Anagram.java
@@ -2,25 +2,23 @@ import java.util.Arrays;
 
 class Anagram {
   public static void main(String[] args) {
-    String str1 = "Race";
-    String str2 = "Care";
+    String str1 = "Listen";
+    String str2 = "Silent";
     
     str1 = str1.toLowerCase();
     str2 = str2.toLowerCase();
 
-    // check if length is same
+
     if(str1.length() == str2.length()) {
 
-      // convert strings to char array
+
       char[] charArray1 = str1.toCharArray();
       char[] charArray2 = str2.toCharArray();
 
-      // sort the char array
+ 
       Arrays.sort(charArray1);
       Arrays.sort(charArray2);
 
-      // if sorted char arrays are same
-      // then the string is anagram
       boolean result = Arrays.equals(charArray1, charArray2);
 
       if(result) {
